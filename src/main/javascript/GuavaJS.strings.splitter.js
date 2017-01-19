e1a778e01e03e7763ff04280e051bbb5705580b7
nerdynick <nerdynick@gmail.com>
2017-01-19 16:36:32 -0700

    Added  Map Splitters. Issue #5

--- src/main/javascript/GuavaJS.strings.splitter.js
+++ src/main/javascript/GuavaJS.strings.splitter.js
@@ -2,25 +2,35 @@
 //Requires GuavaJS.strings.js
 //Requires GuavaJS.strings.charmatcher.js
 
+//Optional if MapSplitter is added
+//Requires GuavaJS.collect.js
+//Requires GuavaJS.collect.iterables.js
+//Requires GuavaJS.collect.multimap.js
+
 GuavaJS.Strings.Splitter = GuavaJS.Strings.Splitter || (function(){
 	var Strings = GuavaJS.Strings;
+	var isString = GuavaJS.isString;
+	
+	var _evalPart = function(trimResults, omitEmptyStrs, part){
+		part = Strings.nullToEmpty(part);
+		
+		if(trimResults){
+			part = Strings.trim(part);
+		}
+		
+		if(!omitEmptyStrs || !Strings.isNullOrEmpty(part)){
+			return part;
+		}
+		
+		return null;
+	}
 	
 	var _splitter = function(charmatcher){
 		var matcher = charmatcher;
 		var trimResults = false;
 		var omitEmptyStrs = false;
 		
-		var evalPart = function(parts, part){
-			part = Strings.nullToEmpty(part);
-			
-			if(trimResults){
-				part = Strings.trim(part);
-			}
-			
-			if(!omitEmptyStrs || !Strings.isNullOrEmpty(part)){
-				parts.push(part);
-			}
-		}
+		var evalPart = function(part){return _evalPart(trimResults, omitEmptyStrs, part);};
 		
 		return {
 			trimResults: function(){
@@ -39,14 +49,83 @@ GuavaJS.Strings.Splitter = GuavaJS.Strings.Splitter || (function(){
 					for(i=0; i<value.length; i++){
 						var char = value.charAt(i);
 						if(matcher.matches(char)){
-							evalPart(parts, part);
+							part = evalPart(part);
+							if(part != null){
+								parts.push(part);
+							}
 							part = "";
 						} else {
 							part += char;
 						}
 					}
 					
-					evalPart(parts, part);
+					part = evalPart(part);
+					if(part != null){
+						parts.push(part);
+					}
+				}
+
+				return parts;
+			},
+			withKeyValueSeparator: function(value){
+				var kvMatcher = value
+				if(isString(value)){
+					kvMatcher = GuavaJS.Strings.CharMatcher.anyOf(value);
+				}
+				
+				return _mapSplitter(kvMatcher, matcher, trimResults, omitEmptyStrs);
+			}
+		}
+	}
+	
+	var _mapSplitter = function(_kvMatcher, _matcher, _trimResults, _omitEmptyStrs){
+		var MultiMap = GuavaJS.Collect.Multimap;
+		
+		var kvMatcher = _kvMatcher;
+		var matcher = _matcher;
+		var trimResults = _trimResults;
+		var omitEmptyStrs = _omitEmptyStrs;
+		
+		var evalPart = function(part){return _evalPart(trimResults, omitEmptyStrs, part);};
+		
+		return {
+			trimResults: function(){
+				trimResults=true; 
+				return this;
+			},
+			omitEmptyStrings: function(){
+				omitEmptyStrs=true; 
+				return this;
+			},
+			split: function(value){
+				var parts = MultiMap.FlatListMultimap();
+				
+				if(!Strings.isNullOrEmpty(value)){
+					var cKey = null;
+					var part = "";
+					for(i=0; i<value.length; i++){
+						var char = value.charAt(i);
+						if(kvMatcher.matches(char)){
+							cKey = part;
+							part = "";
+						} else if(matcher.matches(char)){
+							part = evalPart(part);
+							if(cKey != null && part != null){
+								parts.push(cKey, part);
+							}
+							part = "";
+							cKey = null;
+						} else {
+							part += char;
+						}
+					}
+					
+					part = evalPart(part);
+					if(part != null){
+						if(cKey != null){
+							parts.push(cKey, part);
+						}
+					}
 				}
 
 				return parts;
@@ -55,12 +134,13 @@ GuavaJS.Strings.Splitter = GuavaJS.Strings.Splitter || (function(){
 	}
 	
 	return {
-		on: function(string){
-			var matcher = GuavaJS.Strings.CharMatcher.anyOf(string);
-			return _splitter(matcher);
-		},
-		using: function(charmatcher){
-			return _splitter(charmatcher);
+		on: function(value){
+			if(isString(value)){
+				var matcher = GuavaJS.Strings.CharMatcher.anyOf(value);
+				return _splitter(matcher);
+			} else {
+				return _splitter(value);
+			}
 		}
 	}
 })();
--- src/test/javascript/GuavaJS.strings.splitter.spec.js
+++ src/test/javascript/GuavaJS.strings.splitter.spec.js
@@ -1,48 +1,99 @@
 describe("String Splitter", function(){
-	it("Basic Split", function(){
-		var Splitter = GuavaJS.Strings.Splitter.on('.');
-		var parts = Splitter.split('test.test ..test.');
+	describe("Single Splitter", function(){
+		it("Basic Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('.');
+			var parts = Splitter.split('test.test ..test.');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.length).toEqual(5);
+			expect(parts[0]).toBe('test');
+			expect(parts[1]).toBe('test ');
+			expect(parts[2]).toBe('');
+			expect(parts[3]).toBe('test');
+			expect(parts[4]).toBe('');
+		});
 		
-		expect(parts).not.toBeNull();
-		expect(parts.length).toEqual(5);
-		expect(parts[0]).toBe('test');
-		expect(parts[1]).toBe('test ');
-		expect(parts[2]).toBe('');
-		expect(parts[3]).toBe('test');
-		expect(parts[4]).toBe('');
+		it("Trim Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('.').trimResults();
+			var parts = Splitter.split('test.test . .test');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.length).toEqual(4);
+			expect(parts[0]).toBe('test');
+			expect(parts[1]).toBe('test');
+			expect(parts[2]).toBe('');
+			expect(parts[3]).toBe('test');
+		});
+		
+		it("Omit Empty Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('.').omitEmptyStrings();
+			var parts = Splitter.split('test.test ..test.');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.length).toEqual(3);
+			expect(parts[0]).toBe('test');
+			expect(parts[1]).toBe('test ');
+			expect(parts[2]).toBe('test');
+		});
+		
+		it("Trim and Omit Empty Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('.').trimResults().omitEmptyStrings();
+			var parts = Splitter.split('test.test . .test');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.length).toEqual(3);
+			expect(parts[0]).toBe('test');
+			expect(parts[1]).toBe('test');
+			expect(parts[2]).toBe('test');
+		});
 	});
 	
-	it("Trim Split", function(){
-		var Splitter = GuavaJS.Strings.Splitter.on('.').trimResults();
-		var parts = Splitter.split('test.test . .test');
+	describe("Map Splitter", function(){
+		it("Basic Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('&').withKeyValueSeparator('=');
+			var parts = Splitter.split('a=test&b=test &&c=test&');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.size()).toEqual(3);
+			expect(parts.get('a')).toBe('test');
+			expect(parts.get('b')).toBe('test ');
+			expect(parts.get('c')).toBe('test');
+		});
 		
-		expect(parts).not.toBeNull();
-		expect(parts.length).toEqual(4);
-		expect(parts[0]).toBe('test');
-		expect(parts[1]).toBe('test');
-		expect(parts[2]).toBe('');
-		expect(parts[3]).toBe('test');
-	});
-	
-	it("Omit Empty Split", function(){
-		var Splitter = GuavaJS.Strings.Splitter.on('.').omitEmptyStrings();
-		var parts = Splitter.split('test.test ..test.');
+		it("Trim Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('&').trimResults().withKeyValueSeparator('=');
+			var parts = Splitter.split('a=test&b=test & &c=test&d= &e=');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.size()).toEqual(5);
+			expect(parts.get('a')).toBe('test');
+			expect(parts.get('b')).toBe('test');
+			expect(parts.get('c')).toBe('test');
+			expect(parts.get('d')).toBe('');
+			expect(parts.get('e')).toBe('');
+		});
 		
-		expect(parts).not.toBeNull();
-		expect(parts.length).toEqual(3);
-		expect(parts[0]).toBe('test');
-		expect(parts[1]).toBe('test ');
-		expect(parts[2]).toBe('test');
-	});
-	
-	it("Trim and Omit Empty Split", function(){
-		var Splitter = GuavaJS.Strings.Splitter.on('.').trimResults().omitEmptyStrings();
-		var parts = Splitter.split('test.test . .test');
+		it("Omit Empty Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('&').omitEmptyStrings().withKeyValueSeparator('=');
+			var parts = Splitter.split('a=test&b=test & &c=test&d= &e=');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.size()).toEqual(4);
+			expect(parts.get('a')).toBe('test');
+			expect(parts.get('b')).toBe('test ');
+			expect(parts.get('c')).toBe('test');
+			expect(parts.get('d')).toBe(' ');
+		});
 		
-		expect(parts).not.toBeNull();
-		expect(parts.length).toEqual(3);
-		expect(parts[0]).toBe('test');
-		expect(parts[1]).toBe('test');
-		expect(parts[2]).toBe('test');
+		it("Trim and Omit Empty Split", function(){
+			var Splitter = GuavaJS.Strings.Splitter.on('&').trimResults().omitEmptyStrings().withKeyValueSeparator('=');
+			var parts = Splitter.split('a=test&b=test & &c=test&d= &e=');
+			
+			expect(parts).not.toBeNull();
+			expect(parts.size()).toEqual(3);
+			expect(parts.get('a')).toBe('test');
+			expect(parts.get('b')).toBe('test');
+			expect(parts.get('c')).toBe('test');
+		});
 	});
 });
